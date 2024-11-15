80da54e75d5e17e244b50be6fcd586dd5cdeaa16
BryanEduardo659 <bryanalmora427@gmail.com>
2024-11-15 11:38:44 -0600

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<script>
+var vm = new Vue({
+  el: '#app',
+  data: {
+    txt: '', // Mensaje que se mostrará al buscar
+    inputText: '' // Texto que el usuario escribe
+  },
+  methods: {
+    search: function () {
+      this.txt = this.inputText; 
+    }
+  }
+});
+</script>
+
+<template>
+   <input 
+        v-model="inputText" 
+        @keyup.enter="search" 
+        placeholder="Ingresa el Dato a Buscar" 
+      />
+      <!-- Botón de buscar -->
+      <button @click="search">Buscar</button>
+      <h1>{{ txt }}</h1>
+</template>
